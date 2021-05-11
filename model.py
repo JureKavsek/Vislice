5c4666a68ba65b8cf8c7ab96bd0058a4df5581f1
Jure Kavsek <jure.kavsek@gmail.com>
2021-05-11 15:40:41 +0200

    Delo z datotekami

--- model.py
+++ model.py
@@ -1,15 +1,55 @@
 import random
+import json
 
 STEVILO_DOVOLJENIH_NAPAK = 10
 PRAVILNA_CRKA, PONOVLJENA_CRKA, NAPACNA_CRKA = '+', 'o', '-'
 ZMAGA, PORAZ = 'W', 'X'
 ZACETEK = 'S'
 
+DATOTEKA_ZA_SHRANJEVANJE = 'C:/code/python/git/vislice/podatki.json'
 
 class Vislice:
-    def __init__(self):
-        self.igre = {}
-        self.max_id = 0
+    def __init__(self, zacetne_igre=None, zacetni_id=0):
+        self.igre = zacetne_igre or {}
+        self.max_id = zacetni_id
+
+    def pretvori_v_json_slovar(self):
+        slovar_iger = {}
+
+        for id_igre, (igra, stanje) in self.igre.items():
+            slovar_iger[id_igre] = {
+                Igra.pretvori_v_json_slovar(),
+                stanje
+            }
+
+        return {
+            "max_id": self.max_id,
+            "igre": slovar_iger
+        }
+
+    def zapisi_v_datoteko(self, datoteka):
+        with open(datoteka, 'w') as out_file:
+            json_slovar = self.pretvori_v_json_slovar()
+            json.dump(json_slovar, out_file, indent=2)
+
+    @classmethod
+    def dobi_iz_json_slovarja(cls, slovar):
+        slovar_iger = {}
+        for id_igre, (igra_slovar, stanje) in slovar['igre'].items():
+            slovar_iger[int(id_igre)] = (
+                Igra.dobi_iz_json_slovarja(igra, slovar), 
+                stanje
+            )
+
+        return Vislice(slovar_iger, slovar['max_id'])
+
+    @staticmethod
+    def preberi_iz_datoteke(datoteka):
+        with open(datoteka, 'r') as in_file:
+            json_slovar = json.load(in_file)
+        
+        return Vislice.dobi_iz_json_slovarja(json_slovar)
+
 
     def prost_id_igre(self):
         self.max_id += 1
@@ -34,6 +74,8 @@ class Vislice:
         self.igre[id_igre] = (igra, novo_stanje)
 
         return novo_stanje
+
+
 
 class Igra:
     def __init__(self, geslo, crke=[]):
@@ -85,6 +127,16 @@ class Igra:
                 return PORAZ
             else:
                 return NAPACNA_CRKA
+    
+    def pretvori_v_json_slovar(self):
+        return {
+            "geslo": self.geslo,
+            "crke": self.crke
+        }
+
+    @staticmethod
+    def dobi_iz_json_slovarja(slovar):
+        return Igra(slovar['geslo'], slovar['crke'])
 
 with open('C:/code/python/git/vislice/besede.txt', encoding="utf8") as f:
     bazen_besed = f.read().split()
--- vislice.py
+++ vislice.py
@@ -13,14 +13,27 @@ def index():
 
 @bottle.post('/igra/')
 def nova_igra():
+
+    vislice = model.Vislice.preberi_iz_datoteke(
+        model.DATOTEKA_ZA_SHRANJEVANJE
+    )
+
     id_igre = vislice.nova_igra()
     novi_url = f'/igra/{id_igre}/'
+
+    vislice.zapisi_v_datoteko(model.DATOTEKA_ZA_SHRANJEVANJE)
 
     bottle.redirect(novi_url)
 
 @bottle.get('/igra/<id_igre:int>/')
 def pokazi_igro(id_igre):
+    vislice = model.Vislice.preberi_iz_datoteke(
+        model.DATOTEKA_ZA_SHRANJEVANJE
+    )
+
     trenutna_igra, trenutno_stanje = vislice.igre[id_igre]
+
+    vislice.zapisi_v_datoteko(model.DATOTEKA_ZA_SHRANJEVANJE)
 
     return bottle.template('igra.tpl',
         igra=trenutna_igra, stanje=trenutno_stanje
@@ -28,11 +41,17 @@ def pokazi_igro(id_igre):
 
 @bottle.post('/igra/<id_igre:int>/')
 def ugibaj_na_igri(id_igre):
+    vislice = model.Vislice.preberi_iz_datoteke(
+        model.DATOTEKA_ZA_SHRANJEVANJE
+    )
+
     trenutna_igra = vislice.igre[id_igre]
     ugibana = bottle.request.forms['crka']
 
     vislice.ugibaj(id_igre, ugibana)
 
+    vislice.zapisi_v_datoteko(model.DATOTEKA_ZA_SHRANJEVANJE)
+    
     return bottle.redirect(f'/igra/{id_igre}/')
 
 bottle.run(reloader=True, debug=True)
